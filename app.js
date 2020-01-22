e9012672c75c4d4ab2eb027a232727532aae2090
HyloxalusFallax <hyloxalusfallax@gmail.com>
2020-01-22 20:47:37 +0300

    Init commit

---
+++ app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const ffmpeg = require('fluent-ffmpeg');
+
+ffmpeg.setFfmpegPath('./ffmpeg/bin/ffmpeg.exe');
+ffmpeg.setFfprobePath('./ffmpeg/bin/ffprobe.exe');
+
+const help = "Help:\n\
+	-i/-input + input video file name\n\
+	-o/-output + output audio file name\n\
+	-h/-help to get help";
+
+const args = process.argv.slice(2);
+var input, output;
+
+for (let i=0; i < args.length; i++) {
+	switch(args[i]) {
+		case '-h':
+		case '-help':
+			console.log(help);
+			return;
+			break;
+		case '-i':
+		case '-input':
+			i++;
+			input = args[i]
+			break;
+		case '-o':
+		case '-output':
+			i++;
+			output = args[i]
+			break;
+		default:
+			console.log('Use -h/-help for help');
+			return;
+			break;
+	}
+}
+
+console.log('input: ' + input);
+console.log('output: ' + output);
+
+if (!input) {
+	console.log('You must specify input file!');
+	return;
+}
+if (!output) {
+	console.log('You must specify output file!');
+	return;
+}
+
+ffmpeg(input).format('mp3').audioBitrate('320k').noVideo().output(output)
+	.on('error', function(err) {
+		console.log('An error occurred: ' + err.message);
+	})
+	.on('start', function() {
+    console.log('Processing started!');
+	})
+	.on('progress', function(progress) {
+    if (Math.round(progress.percent)%5 == 0)
+		console.log('Processing: ' + Math.round(progress.percent) + '% done');
+	})
+	.on('end', function() {
+		console.log('Processing finished!');
+	})
+	.run();
